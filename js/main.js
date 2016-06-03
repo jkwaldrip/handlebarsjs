4b1ef37715f025c0c3006734b232ccbdd35c3c63
Jain Waldrip <jkwaldri@iu.edu>
2016-06-03 14:50:50 -0400

    Move main content to JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 // Author's own JS additions here
 $(document).ready(
-  function() {}
+  function() {
+    var initialContent = '<p class="animated fadeInDown">This index.html page is a placeholder with the CSS, font and favicon. It\'s just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>';
+    $('#content div.one-half').append(initialContent);
+  }
 );
