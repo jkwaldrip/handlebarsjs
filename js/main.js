f26c1cf309fda5fee4892e1f85c6dd987cb6b735
Jain Waldrip <jkwaldri@iu.edu>
2016-06-03 15:01:35 -0400

    Use handlebars template & context for main text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 // Author's own JS additions here
 $(document).ready(
   function() {
-    var initialContent = '<p class="animated fadeInDown">This index.html page is a placeholder with the CSS, font and favicon. It\'s just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>';
-    $('#content div.one-half').append(initialContent);
+    var template = '<p class="animated fadeInDown">{{main}}</p>';
+    var context = {'main': 'This index.html page is a placeholder with the CSS, font and favicon. It\'s just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.'}
+    var templateScript = Handlebars.compile(template);
+    var html = templateScript(context);
+
+    $('#content div.one-half').append(html);
   }
 );
