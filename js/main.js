1dd872853d08625885f65294342d6e2f4e880f3f
Jain Waldrip <jkwaldri@iu.edu>
2016-06-03 15:51:32 -0400

    Add formatting to navbar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // Author's own JS additions here
 $(document).ready(
   function() {
-    var template = '<p class="animated fadeInDown">{{main}}</p>';
+    var template = '<p class="animated fadeInDown">{{{main}}}</p>';
     var context = {'main': 'This index.html page is a placeholder with the CSS, font and favicon. It\'s just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.'}
     var templateScript = Handlebars.compile(template);
     var html = templateScript(context);
 
     $('#content div.one-half').append(html);
 
-    var navTemplate = '<nav><ul>' +
+    var navTemplate = '<nav class="one-half column u-pull-right"><ul>' +
         '<li><a href="{{home}}">Home</a></li>' +
         '<li><a href="{{linkTwo}}">Link One</a></li>' +
         '<li><a href="{{linkTwo}}">Link Two</a></li>' +
